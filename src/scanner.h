4e8d2a690610cae96e2fa1cdf9a6ad8a0ab326be
stegnerw <wayne.stegner@protonmail.com>
2021-01-29 21:59:08 -0500

    Moved character types back to scanner.h

--- src/scanner.h
+++ src/scanner.h
@@ -6,6 +6,31 @@
 #include <unordered_map>
 #include "token.h"
 
+enum c_type {
+	C_INVALID,	// Anything not listed below - the default
+	C_UPPER,	// [A-Z]
+	C_LOWER,	// [a-z]
+	C_DIGIT,	// [0-9]
+	C_PERIOD,	// .
+	C_UNDER,	// _
+	C_SEMICOL,	// ;
+	C_COLON,	// :
+	C_COMMA,	// ,
+	C_LPAREN,	// (
+	C_RPAREN,	// )
+	C_LBRACK,	// [
+	C_RBRACK,	// ]
+	C_LBRACE,	// {
+	C_RBRACE,	// }
+	C_EXPR,		// & |
+	C_RELAT,	// < > = !
+	C_ARITH,	// + -
+	C_TERM,		// / *
+	C_QUOTE,	// "
+	C_WHITE,	// space, tab, newline, carriage return
+	C_EOF		// EOF char
+};
+
 class Scanner {
 	public:
 		Scanner();
--- src/token.h
+++ src/token.h
@@ -3,31 +3,6 @@
 
 #include <sstream>
 
-enum c_type {
-	C_INVALID,	// Anything not listed below - the default
-	C_UPPER,	// [A-Z]
-	C_LOWER,	// [a-z]
-	C_DIGIT,	// [0-9]
-	C_PERIOD,	// .
-	C_UNDER,	// _
-	C_SEMICOL,	// ;
-	C_COLON,	// :
-	C_COMMA,	// ,
-	C_LPAREN,	// (
-	C_RPAREN,	// )
-	C_LBRACK,	// [
-	C_RBRACK,	// ]
-	C_LBRACE,	// {
-	C_RBRACE,	// }
-	C_EXPR,		// & |
-	C_RELAT,	// < > = !
-	C_ARITH,	// + -
-	C_TERM,		// / *
-	C_QUOTE,	// "
-	C_WHITE,	// space, tab, newline, carriage return
-	C_EOF		// EOF char
-};
-
 /*
  * Token Types (from page 112):
  * Each keyword gets its own token type
