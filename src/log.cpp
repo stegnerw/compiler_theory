1538f556c7f48242eec6fd3c7c7c150978a188ee
stegnerw <wayne.stegner@protonmail.com>
2021-03-15 21:49:37 -0400

    Added line number recording to the log

--- src/log.cpp
+++ src/log.cpp
@@ -1,21 +1,25 @@
 #include "log.h"
-#include <string>
-#include <sstream>
 
-/*
- * Public
- */
+#include <sstream>
+#include <string>
+
+////////////////////////////////////////////////////////////////////////////////
+// Public
+////////////////////////////////////////////////////////////////////////////////
+
 std::stringstream LOG::ss;
+int LOG::line_number = 0;
 
 LOG::LOG(LOG_LEVEL type) : msg_level(type) {
 	if (!ss.str().empty()) {
 		if (msg_level >= min_level) {
-			std::cout << colors[type] << labels[type] << ss.str() << COL_RST
-				<< std::endl;
+			std::cout << COLORS[type] << LABELS[type] << "Line " << line_number
+				<< std::endl << "\t" << ss.str() << COL_RST << std::endl;
 		}
 		log_fstream.open(log_file, std::ios::app);
 		if (log_fstream) {
-			log_fstream << labels[type] << ss.str() << std::endl;
+			log_fstream << LABELS[type] << "Line " << line_number << std::endl
+				<< "\t" << ss.str() << std::endl;
 			log_fstream.close();
 		}
 		ss.str(std::string());
@@ -36,22 +40,23 @@ bool LOG::setLogFile(std::string f) {
 
 bool LOG::setMinLevel(int l) {
 	switch (l) {
-		case 0:		min_level = DEBUG;	break;
-		case 1:		min_level = INFO;	break;
-		case 2:		min_level = WARN;	break;
-		case 3:		min_level = ERROR;	break;
-		default:	return false;		break;
+		case 0: min_level = DEBUG; break;
+		case 1: min_level = INFO; break;
+		case 2: min_level = WARN; break;
+		case 3: min_level = ERROR; break;
+		default: return false; break;
 	}
 	return true;
 }
 
-/*
- * Private
- */
+////////////////////////////////////////////////////////////////////////////////
+// Private
+////////////////////////////////////////////////////////////////////////////////
+
 LOG_LEVEL LOG::min_level = INFO;
 std::string LOG::log_file;
 std::ofstream LOG::log_fstream;
-const std::string LOG::labels[4] = {"[DEBUG]\t", "[INFO ]\t", "[WARN ]\t",
-	"[ERROR]\t"};
-const std::string LOG::colors[4] = {COL_CYN, COL_WHT, COL_YEL, COL_RED};
+const std::string LOG::LABELS[4] = {"[DEBUG]\t", "[INFO ]\t", "[WARN ]\t",
+		"[ERROR]\t"};
+const std::string LOG::COLORS[4] = {COL_CYN, COL_WHT, COL_YEL, COL_RED};
 
--- src/log.h
+++ src/log.h
@@ -1,43 +1,44 @@
 /*
  * File:		Log.h
  * Author:		Alberto Lepe <dev@alepe.com>
- * Contributor: Wayne Stegner <wayne.stegner@protonmail.com>
+ * Contributor:	Wayne Stegner <wayne.stegner@protonmail.com>
  *
  * This code was found and modified from this post on Stack Overflow:
  * https://stackoverflow.com/questions/5028302/small-logger-class/32262143#32262143
- * I (Wayne Stegner) restructured it and added colors
- * I plan to make it output to a log file as well as printing
+ * I (Wayne Stegner) restructured it and added COLORS
  */
 
 #ifndef LOG_H
 #define LOG_H
 
-#include <string>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <sstream>
+#include <string>
 
 // Terminal color definitions
-#define COL_RST  "\x1B[0m"
-#define COL_CYN  "\x1B[36m"
-#define COL_WHT  "\x1B[37m"
-#define COL_YEL  "\x1B[33m"
-#define COL_RED  "\x1B[31m"
+const std::string COL_RST = "\x1B[0m";
+const std::string COL_CYN = "\x1B[36m";
+const std::string COL_WHT = "\x1B[37m";
+const std::string COL_YEL = "\x1B[33m";
+const std::string COL_RED = "\x1B[31m";
 
 enum LOG_LEVEL {DEBUG, INFO, WARN, ERROR};
 
 class LOG {
 public:
 	static std::stringstream ss;
+	static int line_number;
 	LOG(LOG_LEVEL type);
-	static bool setLogFile(std::string f);
-	static bool setMinLevel(int l);
+	static bool setLogFile(std::string);
+	static bool setMinLevel(int);
+
 private:
 	static LOG_LEVEL min_level;
 	static std::string log_file;
 	static std::ofstream log_fstream;
-	static const std::string labels[4];
-	static const std::string colors[4];
+	static const std::string LABELS[4];
+	static const std::string COLORS[4];
 	LOG_LEVEL msg_level;
 };
 
