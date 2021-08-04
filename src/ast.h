268ae6f50cc89af57fc4a28175c158361125ac74
stegnerw <wayne.stegner@protonmail.com>
2021-08-03 22:41:26 -0400

    Finish basic AST

--- src/ast.h
+++ src/ast.h
@@ -12,18 +12,19 @@ namespace ast {
   // Abstract node for the AST interface
   class Node {
    public:
+    Node() = delete;
     Node(std::string name) : name(name), tm(TYPE_NONE) {}
     Node(std::string name, TypeMark tm) : name(name), tm(tm) {}
     virtual ~Node() = 0;
-    virtual bool typeCheck() = 0;
+    //virtual bool typeCheck() = 0;
+    virtual std::string getName() { return name; }
     TypeMark getTypeMark() { return tm; }
-    std::string getName() { return name; }
 
    protected:
-    const std::string name;
+    std::string name;
     TypeMark tm;
     // TODO: Do I need this? I think not.
-    static std::shared_ptr<Environment> env;
+    //static std::shared_ptr<Environment> env;
   };
 
   template <class T>
@@ -43,40 +44,40 @@ namespace ast {
   // Expression, arith_op, relation, and term ops
   class BinaryOp : public Node {
    public:
-    BinaryOp(std::shared_ptr<Node> lhs, std::shared_ptr<Node> rhs,
+    BinaryOp(std::unique_ptr<Node> lhs, std::unique_ptr<Node> rhs,
         std::shared_ptr<Token> op_tok) :
       Node("Binary Op"),
-      lhs(lhs),
-      rhs(rhs),
+      lhs(std::move(lhs)),
+      rhs(std::move(rhs)),
       op_tok(op_tok) {}
 
    protected:
-    std::shared_ptr<Node> lhs;
-    std::shared_ptr<Node> rhs;
+    std::unique_ptr<Node> lhs;
+    std::unique_ptr<Node> rhs;
     std::shared_ptr<Token> op_tok;
   };
 
   // Not and negative ops
   class UnaryOp : public Node {
    public:
-    UnaryOp(std::shared_ptr<Node> lhs, std::shared_ptr<Token> op_tok) :
+    UnaryOp(std::unique_ptr<Node> lhs, std::unique_ptr<Token> op_tok) :
       Node("Unary Op"),
-      lhs(lhs),
-      op_tok(op_tok) {}
+      lhs(std::move(lhs)),
+      op_tok(std::move(op_tok)) {}
 
    protected:
-    std::shared_ptr<Node> lhs;
-    std::shared_ptr<Token> op_tok;
+    std::unique_ptr<Node> lhs;
+    std::unique_ptr<Token> op_tok;
   };
 
   class VariableReference : public Node { // Destination and Name
    public:
 
     VariableReference(std::shared_ptr<IdToken> id_tok,
-        std::shared_ptr<Node> expr_node) :
+        std::unique_ptr<Node> expr_node) :
       Node("Variable Reference"),
       id_tok(id_tok),
-      expr_node(expr_node) {}
+      expr_node(std::move(expr_node)) {}
 
     VariableReference(std::shared_ptr<IdToken> id_tok) :
       Node("Variable Reference"),
@@ -85,118 +86,205 @@ namespace ast {
 
    protected:
     std::shared_ptr<IdToken> id_tok;
-    std::shared_ptr<Node> expr_node;
+    std::unique_ptr<Node> expr_node;
   };
 
   class AssignmentStatement : public Node {
    public:
-    AssignmentStatement(std::shared_ptr<VariableReference> dest,
-        std::shared_ptr<Node> expr) :
-      Node("Assignment Statement") {}
+    AssignmentStatement(std::unique_ptr<VariableReference> dest,
+        std::unique_ptr<Node> expr) :
+      Node("Assignment Statement"),
+      dest(std::move(dest)),
+      expr(std::move(expr)) {}
 
    protected:
-    std::shared_ptr<VariableReference> dest;
-    std::shared_ptr<Node> expr;
+    std::unique_ptr<VariableReference> dest;
+    std::unique_ptr<Node> expr;
   };
 
   class IfStatement : public Node {
    public:
-    IfStatement(std::shared_ptr<Node> if_cond, std::shared_ptr<Node> then_stmt,
-        std::shared_ptr<Node> else_stmt) :
+    IfStatement(std::unique_ptr<Node> if_cond, std::unique_ptr<Node> then_stmt,
+        std::unique_ptr<Node> else_stmt) :
       Node("If Statement"),
-      if_cond(if_cond),
-      then_stmt(then_stmt),
-      else_stmt(else_stmt) {}
+      if_cond(std::move(if_cond)),
+      then_stmt(std::move(then_stmt)),
+      else_stmt(std::move(else_stmt)) {}
 
    protected:
-    std::shared_ptr<Node> if_cond, then_stmt, else_stmt;
+    std::unique_ptr<Node> if_cond, then_stmt, else_stmt;
   };
 
   class LoopStatement : public Node {
    public:
-    LoopStatement(std::shared_ptr<AssignmentStatement> assign,
-        std::shared_ptr<Node> expr, std::shared_ptr<Node> stmt) :
+    LoopStatement(std::unique_ptr<AssignmentStatement> assign,
+        std::unique_ptr<Node> expr, std::unique_ptr<Node> stmt) :
       Node("Loop Statement"),
-      assign(assign),
-      expr(expr),
-      stmt(stmt) {}
+      assign(std::move(assign)),
+      expr(std::move(expr)),
+      stmt(std::move(stmt)) {}
 
    protected:
-    std::shared_ptr<AssignmentStatement> assign;
-    std::shared_ptr<Node> expr, stmt;
+    std::unique_ptr<AssignmentStatement> assign;
+    std::unique_ptr<Node> expr, stmt;
   };
 
-  // expr
   class ReturnStatement : public Node {
    public:
-  };
-
-  // expr, arg_list
-  class ArgumentList : public Node {
-   public:
-  };
-
-  // id, arg_list
-  class ProcedureCall : public Node {
-   public:
-  };
-
-  // id, tm, number (array size)
-  class VariableDeclaration : public Node {
-   public:
-  };
-
-  // declaration(s), statement(s)
-  class ProcedureBody : public Node {
-   public:
-  };
-
-  // var_decl, param_list
-  class ParameterList : public Node {
-   public:
-  };
-
-  // id, tm, param_list
-  class ProcedureHeader : public Node {
-   public:
-  };
-
-  // procedure_header, procedure_body
-  class ProcedureDeclaration : public Node {
-   public:
-  };
-
-  // statement, next_statement
-  class Statement : public Node {
-   public:
-  };
-
-  // global, declaration (either proc or var), next_decl
-  class Declaration : public Node {
-   public:
-  };
-
-  // declaration(s), statement(s)
-  class ProgramBody : public Node {
-   public:
+    ReturnStatement(std::unique_ptr<Node> expr) :
+      Node("Return Statement"),
+      expr(std::move(expr)) {}
 
    protected:
-    std::shared_ptr<Declaration> decl;
-    std::shared_ptr<Statement> stmt;
+    std::unique_ptr<Node> expr;
   };
 
-  // id, body
-  class Prog : public Node {
+  class ArgumentList : public Node {
    public:
-    Prog(std::shared_ptr<IdToken> id_tok,
-        std::shared_ptr<ProgramBody> prog_body) :
-      Node("Program"),
+    ArgumentList(std::shared_ptr<Node> expr,
+        std::unique_ptr<ArgumentList> arg_list) :
+      Node("Argument List"),
+      expr(expr),
+      arg_list(std::move(arg_list)) {}
+
+   protected:
+    std::shared_ptr<Node> expr;
+    std::unique_ptr<ArgumentList> arg_list;
+  };
+
+  class ProcedureCall : public Node {
+   public:
+    ProcedureCall(std::shared_ptr<IdToken> id_tok,
+        std::unique_ptr<ArgumentList> arg_list) :
+      Node("Procedure Call"),
       id_tok(id_tok),
-      prog_body(prog_body) {}
+      arg_list(std::move(arg_list)) {}
 
    protected:
     std::shared_ptr<IdToken> id_tok;
-    std::shared_ptr<ProgramBody> prog_body;
+    std::unique_ptr<ArgumentList> arg_list;
+  };
+
+  // id, number (array size)
+  class VariableDeclaration : public Node {
+   public:
+    VariableDeclaration(std::shared_ptr<IdToken> id_tok,
+        std::unique_ptr<Literal<int>> bound) :
+      Node("Variable Declaration"),
+      id_tok(id_tok),
+      bound(std::move(bound)) {}
+
+   protected:
+    std::shared_ptr<IdToken> id_tok;
+    std::unique_ptr<Literal<int>> bound;
+  };
+
+  class Statement : public Node {
+   public:
+    Statement(std::unique_ptr<Node> stmt) :
+      Node("Statement"),
+      stmt(std::move(stmt)),
+      next_stmt(nullptr) {}
+    void setNext(std::unique_ptr<Statement> next) {
+      next_stmt = std::move(next);
+    }
+
+   protected:
+    std::unique_ptr<Node> stmt;
+    std::unique_ptr<Statement> next_stmt;
+  };
+
+  class Declaration : public Node {
+   public:
+    Declaration(bool global, std::unique_ptr<Node> decl) :
+      Node("Declaration"),
+      global(global),
+      decl(std::move(decl)),
+      next_decl(nullptr) {}
+    void setNext(std::unique_ptr<Declaration> next) {
+      next_decl = std::move(next);
+    }
+
+   protected:
+    bool global;
+    std::unique_ptr<Node> decl;
+    std::unique_ptr<Declaration> next_decl;
+  };
+
+  class ProcedureBody : public Node {
+   public:
+    ProcedureBody(std::unique_ptr<Declaration> decl,
+        std::unique_ptr<Statement> stmt) :
+      Node("Procedure Body"),
+      decl(std::move(decl)),
+      stmt(std::move(stmt)) {}
+
+   protected:
+    std::unique_ptr<Declaration> decl;
+    std::unique_ptr<Statement> stmt;
+  };
+
+  class ParameterList : public Node {
+   public:
+    ParameterList(std::unique_ptr<VariableDeclaration> var_decl,
+        std::unique_ptr<ParameterList> param_list) :
+      Node("Parameter List"),
+      var_decl(std::move(var_decl)),
+      param_list(std::move(param_list)) {}
+
+   protected:
+    std::unique_ptr<VariableDeclaration> var_decl;
+    std::unique_ptr<ParameterList> param_list;
+  };
+
+  class ProcedureHeader : public Node {
+   public:
+    ProcedureHeader(std::shared_ptr<IdToken> id_tok, TypeMark tm,
+        std::unique_ptr<ParameterList> param_list) :
+      Node("Procedure Header", tm), id_tok(id_tok) {}
+
+   protected:
+    std::shared_ptr<IdToken> id_tok;
+    std::unique_ptr<ParameterList> param_list;
+  };
+
+  class ProcedureDeclaration : public Node {
+   public:
+    ProcedureDeclaration(std::unique_ptr<ProcedureHeader> proc_head,
+        std::unique_ptr<ProcedureBody> proc_body) :
+      Node("Procedure Declaration"),
+      proc_head(std::move(proc_head)),
+      proc_body(std::move(proc_body)) {}
+
+   protected:
+    std::unique_ptr<ProcedureHeader> proc_head;
+    std::unique_ptr<ProcedureBody> proc_body;
+  };
+
+  class ProgBody : public Node {
+   public:
+    ProgBody(std::unique_ptr<Declaration> decl,
+        std::unique_ptr<Statement> stmt) :
+      Node("Program Body"),
+      decl(std::move(decl)),
+      stmt(std::move(stmt)) {}
+
+   protected:
+    std::unique_ptr<Declaration> decl;
+    std::unique_ptr<Statement> stmt;
+  };
+
+  class Prog : public Node {
+   public:
+    Prog(std::shared_ptr<IdToken> id_tok,
+        std::unique_ptr<ProgBody> prog_body) :
+      Node("Program"),
+      id_tok(id_tok),
+      prog_body(std::move(prog_body)) {}
+
+   protected:
+    std::shared_ptr<IdToken> id_tok;
+    std::unique_ptr<ProgBody> prog_body;
   };
 
 } // namespace ast
